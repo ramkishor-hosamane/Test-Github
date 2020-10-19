d4bad17f16c78d95b0eb244d9185ee7e1bec9371
Ram Kishor <ramhosamane@gmail.com>
2020-10-19 12:23:47 +0530

    changing to blue

--- set_git_credentials.sh
+++ set_git_credentials.sh
@@ -2,12 +2,22 @@ echo "Github Configuration"
 echo "--------------------"
 sleep 1
 echo
+read -p "Enter your name : " name
+read -p "Enter your email id (one which is configured with github) : " email
+git config --global user.email "$email"
+git config --global user.name "$name"
+
+echo "[+] Configured your account's default identity"
+
+
 read -p "Enter your github username : " username
 read -s -p "Enter your github password : " password
-echo -p "$pass" | sed 's/./*/g'
-read -p "Enter github repository username : " git_repo_username
-read -p "Enter github repository name : " repository
+
+read -p "Enter remote github repository username : " git_repo_username
+read -p "Enter remote github repository name : " repository
 
 
 git remote set-url origin https://$username:$password@github.com/$git_repo_username/$repository.git
+echo "[+] Configured remote Repository"
+
 echo "Done!!!!"
